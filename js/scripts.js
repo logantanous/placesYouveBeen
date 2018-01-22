dd3449a782b5bfea5a1349a3dc0589307f2ec880
Logan Tanous <logantanous@gmail.com>
2018-01-22 14:11:42 -0800

    add logic for adding after every row

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
   var width = $(window).width();
 
   $("button").click(function() {
-    width = $(window).width();
+    width = $(window).width()-40;
     var maxAmountOfRowItems = parseInt(width/300);
     //itemsCreated++;
     var location = $("#location").val();
@@ -24,38 +24,24 @@ $(document).ready(function(){
     var notes = $("#notes").val();
     var newDestination = new destination(location, landmarks, notes, image);
     destinationArray.push(newDestination);
-     $(".results").append("<div class='items'>"+
-       "<img class='place' src='"+newDestination.image+"' alt='"+location+"'>");
-
-      $(".items").last().click(function() {
-        var indexOfItem = $(this).index();
-        var elements = $(".items");
-        var amountOfElements = elements.length+1;
-        var rowcount = maxAmountOfRowItems;
-        alert("rowcount: "+rowcount);
-        for (var i = 0; i <= amountOfElements; i++) {
+    $(".results").append("<div class='items'>"+"<img class='place' src='"+newDestination.image+"' alt='"+location+"'>");
+    $(".items").last().click(function() {
+      var indexOfItem = $(this).index();
+      var elements = $(".items");
+      var amountOfElements = elements.length+1;
+      var rowcount = maxAmountOfRowItems;
+      alert("rowcount: "+rowcount+" amountOfElements: "+elements.length);
+      for (var i = 0; i <= amountOfElements; i++) {
           console.log(i);
-          if (i==rowcount) {
-            console.log("yup");
-            rowcount = rowcount + maxAmountOfRowItems;
-            $(".items:nth-child("+rowcount+")").after("<p>test</p>");
-          }
+        if (i==rowcount) {
+          $(".items:nth-child("+rowcount+")").after("<p>test</p>");
+          rowcount = rowcount + maxAmountOfRowItems + 1;
         }
-      });
-
-
-        //alert(maxAmountOfRowItems);
-        /*$(".items:nth-child("+maxAmountOfRowItems+")").after("<div class='info'>"+
-          "<img class='place' src='"+newDestination.image+"' alt='"+newDestination.location+"'>"+
-          "<div class='hidden'>"+
-            "<h3>"+newDestination.location+"</h3>"+
-            "<p>"+newDestination.landmarks+"</p>"+
-            "<p>"+newDestination.notes+"</p>"+
-            "</div>"+
-          "</div>");*/
-      })
+      }
+      if (elements.length % maxAmountOfRowItems === 0) {}
+      else {
+        $(".results").append("<p>test</p>");
+      }
+    });
   })
-  // $("p").click(function() {
-  //   alert("hello");
-  //   // $(this).next().css("background-color", "red");
-  // })
+})
